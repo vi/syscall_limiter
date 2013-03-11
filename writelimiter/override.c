30bdcf1daaca546ac0a554ac088c070d3873c822
Vitaly _Vi Shukela <vi0oss@gmail.com>
2013-03-11 16:52:43 +0300

    writelimiter: Refactor library a bit

--- writelimiter/override.c
+++ writelimiter/override.c
@@ -20,51 +20,68 @@
 #include "recv_fd.h"
 #include "send_fd.h"
 
-static int (*orig_open)(const char *pathname, int flags, mode_t mode) = NULL;
-static int (*orig_open64)(const char *pathname, int flags, mode_t mode) = NULL;
-static int (*orig_creat)(const char *pathname, mode_t mode) = NULL;
-static int (*orig_creat64)(const char *pathname, mode_t mode) = NULL;
+#define SOCKET 33
+#define TICKET_SOCKET 34
 
-int remote_open(const char *pathname, int flags, mode_t mode) {
+static int absolutize_path(struct request *r, const char* pathname) {
+    if(pathname[0]!='/') {
+        // relative path
+        char* ret = getcwd(r->pathname, sizeof(r->pathname));
+        if (!ret) {
+            return -1;
+        }
+        int l = strlen(r->pathname);
+        if (l==PATH_MAX) {
+            errno=ERANGE;
+            return -1;
+        }
+        r->pathname[l]='/';
+        strncpy(r->pathname+l+1, pathname, PATH_MAX-l-1);
+    } else {
+        // absolute path
+        strncpy(r->pathname, pathname, PATH_MAX);
+    }
+    return 0;
+}
+
+static int receive_ticket() {
     char buf[6];
-    /* receive a ticket for transaction */
-    read(34, buf, 6);
+    return read(TICKET_SOCKET, buf, 6);
+}
+
+static int perform_request(const struct request* r) {
+    write(SOCKET, r, sizeof(*r));
+    int ret = read(SOCKET, &errno, sizeof(errno));
+    if(ret==-1)return -1;
+    if(errno) {
+        return -1;
+    } else {
+        int ret = 0;
+        if (r->operation=='o') {
+            ret = recv_fd(SOCKET);
+        }
+        return ret;
+    }
+}
+
+static int remote_open(const char *pathname, int flags, mode_t mode) {
+    receive_ticket();
 
     struct request r;
     r.operation = 'o';
     r.flags = flags;
     r.mode = mode;
     
-    if(pathname[0]!='/') {
-        // relative path
-        char* ret = getcwd(r.pathname, sizeof(r.pathname));
-        if (!ret) {
-            return -1;
-        }
-        int l = strlen(r.pathname);
-        if (l==PATH_MAX) {
-            errno=ERANGE;
-            return -1;
-        }
-        r.pathname[l]='/';
-        strncpy(r.pathname+l+1, pathname, PATH_MAX-l-1);
-    } else {
-        // absolute path
-        strncpy(r.pathname, pathname, PATH_MAX);
-    }
+    int ret = absolutize_path(&r, pathname);    
+    if (ret==-1) return -1;
+        
+    ret=-1;
     
-    
-    int ret = -1;
-    
-    write(33, &r, sizeof(r));
-    read(33, &errno, sizeof(errno));
-    if (!errno) {
-        ret = recv_fd(33);
-    }
-    return ret;
+    return perform_request(&r);
 }
 
 
+static int (*orig_open)(const char *pathname, int flags, mode_t mode) = NULL;
 int open(const char *pathname, int flags, mode_t mode) {
     if(!orig_open) {
         orig_open = dlsym(RTLD_NEXT, "open");
@@ -77,6 +94,7 @@ int open(const char *pathname, int flags, mode_t mode) {
     return ret;
 }
 
+static int (*orig_open64)(const char *pathname, int flags, mode_t mode) = NULL;
 int open64(const char *pathname, int flags, mode_t mode) {
     if(!orig_open64) {
         orig_open64 = dlsym(RTLD_NEXT, "open64");
@@ -89,6 +107,7 @@ int open64(const char *pathname, int flags, mode_t mode) {
     return ret;
 }
 
+static int (*orig_creat)(const char *pathname, mode_t mode) = NULL;
 int creat(const char *pathname, mode_t mode) {
     if(!orig_creat) {
         orig_creat = dlsym(RTLD_NEXT, "creat");
@@ -101,6 +120,7 @@ int creat(const char *pathname, mode_t mode) {
     return ret;    
 }
 
+static int (*orig_creat64)(const char *pathname, mode_t mode) = NULL;
 int creat64(const char *pathname, mode_t mode) {
     if(!orig_creat64) {
         orig_creat64 = dlsym(RTLD_NEXT, "creat64");
